507602a2928a11d8ede410d449bd555fcd5b4ec4
chanpreet Singh <chanpreetsingh73553@gmail.com>
2026-02-01 12:05:12 +0530

    add headlessui and lucide icons

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,8 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1 class="text-2xl font-bold pb-0.5">PBook Shop</h1>
+  <p>Vue js + Tailwind Css</p>
 </template>
 
 <style scoped></style>
